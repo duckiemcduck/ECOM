467d8c7dd0180479af9659ac457372ea6f23160a
Aluno <lucascoliado@hotmail.com>
2017-08-02 17:07:58 -0300

    Arquivos Iniciais

---
+++ curl.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -L https://github.com/docker/compose/releases/download/1.15.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
---
+++ install-compose.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo ./curl.sh
+sudo chmod +x /usr/local/bin/docker-compose
